d06b5d00e6c282b77f9d09b99229ea69a25a9843
ckester99 <CKESTER@MURRAYSTATE.EDU>
2020-09-27 17:17:19 -0500

    Add files via upload

--- TestPuzzle.py
+++ TestPuzzle.py
@@ -1,11 +1,13 @@
 import GA2
 
 """
-0 0 3
-  ^
-2 0 0
+1 0>0>0
+  
+0 2 0 0
      
-0 1<0
+0 0<0 0
+
+0 0 0 3
 
 
 
@@ -15,8 +17,11 @@ import GA2
 """
 
 alg = GA2.GA2() #alg is a GA2
-alg.staticNums = [[0,2,3],[2,1,1],[1,0,2]] #this specific GA2's staticNums are these
-alg.logic = [[1,1,0,1],[2,2,2,1]]
+alg.staticNums = [[0,0,1],[1,1,2],[3,3,3]] #this specific GA2's staticNums are these
+alg.logic = [[0,1,0,2],[0,2,0,3],[2,2,2,1]]
+alg.puzzleSize = 4
+alg.mutationRate = .05
+alg.popSize = 50
 
 alg.populate(1)
 """
@@ -26,10 +31,14 @@ for n in range(10000):
 		print(n)
 """
 n = 0
-while not (max(alg.fitness) == 10 or n >= 10000):
+while not (max(alg.fitness) == 10 or n >= 100000):
 	alg.populate()
 	if not n%100:
 		print(n)
+	n += 1
+	if n > 1000:
+		alg.populate(1)
+		n = 0
 
 best = alg.population[alg.fitness.index(max(alg.fitness))]
 for n in best:
